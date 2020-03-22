2c2fb1d91e785e02222ce96b7a414d71d32b1655
ChabaJosa <chabagjg@gmail.com>
2020-03-22 16:56:57 -0400

    Changed Kata

--- Strings-Is-Kata.js
+++ Strings-Is-Kata.js
@@ -1,13 +1,5 @@
 function solution(str, ending){
-    
-
-    if(str.includes(ending) && str.slice(-1) === ending.slice(-1)) {
-        console.log(true) 
-        console.log("str",str.slice(-1)) 
-        console.log("ending",ending.slice(-1)) 
-
-    } else {console.log(false);} 
-
-}
+    return str.endsWith(ending);
+  }
 
 solution("Samurai","rai")
